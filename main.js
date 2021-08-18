affde29325b06e7d81a5e17ad614cb8573c1f7ee
Natalia Zamboni Vergara <nzamboniv@gmail.com>
2021-08-18 15:32:36 -0600

    Refactor code for readability & style guidelines

--- main.js
+++ main.js
@@ -1,132 +1,148 @@
 var recipeTypes = {
   sides: [
-  "Miso Glazed Carrots",
-  "Coleslaw",
-  "Garden Salad",
-  "Crispy Potatoes",
-  "Sweet Potato Tots",
-  "Coconut Rice",
-  "Caeser Salad",
-  "Shrimp Summer Rolls",
-  "Garlic Butter Mushrooms",
-  "Hush Puppies"
+    'Miso Glazed Carrots',
+    'Coleslaw',
+    'Garden Salad',
+    'Crispy Potatoes',
+    'Sweet Potato Tots',
+    'Coconut Rice',
+    'Caeser Salad',
+    'Shrimp Summer Rolls',
+    'Garlic Butter Mushrooms',
+    'Hush Puppies'
   ],
 
   entrees: [
-  "Spaghetti and Meatballs",
-  "Pineapple Chicken",
-  "Shakshuka",
-  "Thai Yellow Curry",
-  "Bibimbap",
-  "Chicken Parmesean",
-  "Butternut Squash Soup",
-  "BBQ Chicken Burgers",
-  "Ramen",
-  "Empanadas",
-  "Chicken Fried Rice",
-  "Sheet Pan Fajitas",
-  "Margarita Pizza"
-],
+    'Spaghetti and Meatballs',
+    'Pineapple Chicken',
+    'Shakshuka',
+    'Thai Yellow Curry',
+    'Bibimbap',
+    'Chicken Parmesean',
+    'Butternut Squash Soup',
+    'BBQ Chicken Burgers',
+    'Ramen',
+    'Empanadas',
+    'Chicken Fried Rice',
+    'Sheet Pan Fajitas',
+    'Margarita Pizza'
+  ],
 
   desserts: [
-  "Apple Pie",
-  "Lemon Meringue Pie",
-  "Black Forest Cake",
-  "Banana Bread",
-  "Peach Cobbler",
-  "Cheesecake",
-  "Funfetti Cake",
-  "Baklava",
-  "Flan",
-  "Macarons",
-  "Macaroons",
-  "Chocolate Cupcakes",
-  "Pavlova",
-  "Pumpkin Pie",
-  "Key Lime Pie",
-  "Tart Tatin",
-  "Croissants",
-  "Eclairs"
+    'Apple Pie',
+    'Lemon Meringue Pie',
+    'Black Forest Cake',
+    'Banana Bread',
+    'Peach Cobbler',
+    'Cheesecake',
+    'Funfetti Cake',
+    'Baklava',
+    'Flan',
+    'Macarons',
+    'Macaroons',
+    'Chocolate Cupcakes',
+    'Pavlova',
+    'Pumpkin Pie',
+    'Key Lime Pie',
+    'Tart Tatin',
+    'Croissants',
+    'Eclairs'
   ]
 }
 
-var currentFood;
-var currentRecypeType;
-var mealTypeForm = document.getElementById("type-form");
-var inputValues = document.getElementsByName("meal-type");
-var randomDishSection = document.getElementById("random-dish");
-var randomDishResult = document.getElementById("random-dish-result")
-var clearButton = document.getElementById("clear-button");
-var cookpotImage = document.getElementById("cookpot-icon");
-var addRecipeButton = document.getElementById("add-recipe");
-var addNewButton = document.getElementById("add-new-button");
-var newRecipeForm = document.getElementById("add-new-form");
-var recipeTypeInput = document.getElementById("recipe-type");
-var recipeNameInput = document.getElementById("recipe-name");
-var newRecipeSection = document.getElementById("new-recipe-form-box");
-var entireMealNode = document.getElementById("entire-meal-label");
+
+var mealTypeForm = document.getElementById('type-form');
+var inputValues = document.getElementsByName('meal-type');
+var randomDishResult = document.getElementById('random-dish-result')
+var clearButton = document.getElementById('clear-button');
+var cookpotImage = document.getElementById('cookpot-icon');
+var addRecipeButton = document.getElementById('add-recipe');
+var addNewButton = document.getElementById('add-new-button');
+var newRecipeForm = document.getElementById('add-new-form');
+var recipeTypeInput = document.getElementById('recipe-type');
+var recipeNameInput = document.getElementById('recipe-name');
+var newRecipeSection = document.getElementById('new-recipe-form-box');
+var entireMealNode = document.getElementById('entire-meal-label');
+var currentFood = '';
+var currentRecipeType = '';
 
 mealTypeForm.addEventListener('submit', getInputValue);
 clearButton.addEventListener('click', clearView);
-addRecipeButton.addEventListener('click', displayAddNewForm);
+addRecipeButton.addEventListener('click', function () {
+  show(newRecipeSection);
+});
 addNewButton.addEventListener('click', saveUserRecipe);
 
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
-};
+}
+
+function show(element) {
+  element.classList.remove('hidden');
+}
+
+function hide(element) {
+  element.classList.add('hidden');
+}
 
 function getInputValue() {
   event.preventDefault();
-  for (var i =0; i < inputValues.length; i++) {
-    if (inputValues[i].checked) {
-      currentRecipeType = inputValues[i].value;
+  inputValues.forEach(inputValue => {
+    if (inputValue.checked) {
+      currentRecipeType = inputValue.value;
     }
-  }
-  randomFood()
-};
+  })
+  randomFood();
+}
 
 function randomFood() {
-  event.preventDefault();
-  if (recipeTypes[currentRecipeType]) {
+  if (!currentRecipeType) {
+    window.alert('Choose a meal type!');
+    return
+  } else if (recipeTypes[currentRecipeType]) {
     currentFood = recipeTypes[currentRecipeType][getRandomIndex(recipeTypes[currentRecipeType])];
-  } else if (currentRecipeType === "entire-meal") {
+  } else if (currentRecipeType === 'entire-meal') {
     currentFood = `${recipeTypes.entrees[getRandomIndex(recipeTypes.entrees)]} with a side of ${recipeTypes.sides[getRandomIndex(recipeTypes.sides)]} and ${recipeTypes.desserts[getRandomIndex(recipeTypes.desserts)]} for dessert`
   }
   renderRandomFood();
-};
+}
 
 function renderRandomFood() {
-  clearButton.classList.remove("hidden");
-  cookpotImage.classList.add("hidden");
-  randomDishResult.innerHTML = `<p>You should make: <p class="random-result">${currentFood}!</p></p>`;
-};
+  show(clearButton);
+  hide(cookpotImage);
+  randomDishResult.innerHTML =
+  `<p>You should make: <p class='random-result'>${currentFood}!</p></p>`;
+}
 
 function clearView() {
-  clearButton.classList.add("hidden");
-  cookpotImage.classList.remove("hidden");
-  randomDishResult.innerHTML = "";
+  hide(clearButton);
+  show(cookpotImage);
+  randomDishResult.innerHTML = '';
+  currentRecipeType = '';
   mealTypeForm.reset();
-};
-
-function displayAddNewForm() {
-  newRecipeSection.classList.remove("hidden");
-};
+}
 
 function saveUserRecipe() {
   event.preventDefault();
   var recipeType = recipeTypeInput.value.toLowerCase();
-  if (recipeTypes[recipeType]) {
+  if (!recipeType || !recipeNameInput.value) {
+    window.alert('Fill out all fields!');
+    return
+  } else if (recipeTypes[recipeType]) {
     recipeTypes[recipeType].push(recipeNameInput.value);
   } else {
     recipeTypes[recipeType] = [recipeNameInput.value];
     addNewMealType()
   }
   currentFood = recipeNameInput.value;
-  newRecipeSection.classList.add("hidden");
+  hide(newRecipeSection);
   newRecipeForm.reset();
   renderRandomFood();
-};
+}
 
 function addNewMealType() {
-  entireMealNode.insertAdjacentHTML("beforebegin", `<label for=${recipeTypeInput.value}><input type="radio" name="meal-type" id=${recipeTypeInput.value} value=${recipeTypeInput.value.toLowerCase()} class="form"/> ${recipeTypeInput.value}</label>`);
-};
+  entireMealNode.insertAdjacentHTML('beforebegin',
+    `<label for=${recipeTypeInput.value}>
+  <input type='radio' name='meal-type' id=${recipeTypeInput.value} value=${recipeTypeInput.value.toLowerCase()} class='form'/>
+   ${recipeTypeInput.value}</label>`);
+}
